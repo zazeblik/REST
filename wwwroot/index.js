3ebe465a2603009f34726111e473b986db35335f
zazeblik <zazeblik@mail.ru>
2020-04-07 21:22:04 +0300

    Добавил AJAX запросы

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,38 +1,62 @@
-function loadBooks() {
-    var books = [{
-        "id": 1,
-        "title": "qqqqq",
-        "author": "qqqq"
-    }, {
-        "id": 2,
-        "title": "www",
-        "author": "www"
-    }]
-    renderTable(books);
+async function loadBooks() {
+    var response = await fetch("http://localhost:3000/api/books");
+    
+    if (response.ok) {
+        var books = await response.json();
+        renderTable(books);
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
 }
 
-function editBook() {
+async function editBook() {
     var id = document.getElementById('editIdInput').value;
     if (!id) return;
     var title = document.getElementById('editTitleInput').value;
     var author = document.getElementById('editAuthorInput').value;
-    // здесь будет код который будет редактировать книгу
-    loadBooks();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('author', author);
+    var response = await fetch("http://localhost:3000/api/books/" + id, {
+        method: 'PUT',
+        body: formData
+    });
+    if (response.ok) {
+        await loadBooks();
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
 }
 
 async function deleteBook() {
     var id = document.getElementById('deleteIdInput').value;
     if (!id) return;
-    // здесь будет код который будет удалять книгу
-    loadBooks();
+    var response = await fetch("http://localhost:3000/api/books/" + id, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        await loadBooks();
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
 }
 
 async function addBook() {
     var title = document.getElementById('addTitleInput').value;
     if (!title) return;
     var author = document.getElementById('addAuthorInput').value;
-    // здесь будет код который будет добавлять книгу
-    loadBooks();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('author', author);
+    var response = await fetch("http://localhost:3000/api/books", {
+        method: 'POST',
+        body: formData
+    });
+    if (response.ok) {
+        await loadBooks();
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
 }
 
 function renderTable(books) {
